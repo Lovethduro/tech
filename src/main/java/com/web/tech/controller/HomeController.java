4a349b27fc7c9d018abd2f4a900d5f6a5a5b280c
Lovethduro <lovethdurodoye@gmail.com>
2025-05-22 23:54:02 +0100

    commit

--- src/main/java/com/web/tech/controller/HomeController.java
+++ src/main/java/com/web/tech/controller/HomeController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // Mapping the root URL to redirect to '/landing'
+    @GetMapping("/")
+    public String redirectToLanding() {
+        return "redirect:/landing"; // This will send users to '/landing'
+    }
+
+    // Mapping '/landing' to the landing page
     @GetMapping("/landing")
-    public String home(Model model) {
-        return "pages/landing";
+    public String landingPage(Model model) {
+        return "pages/landing";  // Refers to 'src/main/resources/templates/pages/landing.html'
     }
 }
